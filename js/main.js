ef1075ffbd923f243c0ec48dc2d7d0ea93dbcbe9
EunhoAndMarie <122677201+EunhoAndMarie@users.noreply.github.com>
2023-03-22 01:08:42 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -84,11 +84,60 @@
 		$(".wedding-loader").fadeOut("slow");
 	};
 
+	var goToTop2 = function() {
+
+		$(window).scroll(function(){
+
+			var $win = $(window);
+			if ($win.scrollTop() > 200) {
+				$('.js-top2').addClass('active');
+			} else {
+				$('.js-top2').removeClass('active');
+			}
+
+		});
+	
+	};
+
 	$(function(){
 		contentWayPoint();
 		testimonialCarousel();
 		goToTop();
 		loaderPage();
+		goToTop2();
+
+
+		var sections = $('section')
+            , nav = $('nav')
+            , nav_height = nav.outerHeight();
+
+            $(window).on('scroll', function () {
+                var cur_pos = $(this).scrollTop();
+  
+            sections.each(function() {
+                var top = $(this).offset().top - nav_height,
+                bottom = top + $(this).outerHeight();
+    
+            if (cur_pos >= top && cur_pos <= bottom) {
+                nav.find('a').removeClass('active');
+                sections.removeClass('active');
+      
+                $(this).addClass('active');
+                nav.find('a[href="#'+$(this).attr('id')+'"]').addClass('active');
+            }
+            });
+            });
+            
+            nav.find('a').on('click', function () {
+              var $el = $(this)
+                , id = $el.attr('href');
+              
+              $('html, body').animate({
+                scrollTop: $(id).offset().top - nav_height
+              }, 500);
+              
+              return false;
+            });
 	});
 
 
